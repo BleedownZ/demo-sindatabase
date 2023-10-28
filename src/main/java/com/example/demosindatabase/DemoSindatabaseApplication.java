8f209f741cd2133d6483b1f2597622b2d03b7abc
Bleedowz <g.6578@live.com>
2023-10-28 20:47:31 +0200

    first commit

---
+++ src/main/java/com/example/demosindatabase/DemoSindatabaseApplication.java
@@ -0,0 +1,13 @@
+package com.example.demosindatabase;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoSindatabaseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoSindatabaseApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/demosindatabase/DemoSindatabaseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.demosindatabase;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoSindatabaseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
